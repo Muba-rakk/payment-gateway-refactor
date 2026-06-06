b5a9468dd1e492f7ab823673cc2ee108dc25a09a
Winata Fadillah Mubarak <winatafadil1177@gmail.com>
2026-06-07 00:30:30 +0700

    tambah titik masuk aplikasi untuk testing

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import payment.PaymentProcessor;
+import payment.interfaces.PaymentMethod;
+import payment.interfaces.PaymentNotifier;
+import payment.interfaces.PaymentRepository;
+import payment.methods.CreditCardPayment;
+import payment.methods.GoPayPayment;
+import payment.services.DatabaseRepository;
+import payment.services.MockEmailNotifier;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Payment Gateway Refactored (Starter Template) ===");
+
+        // TODO: Setup Services
+        // - Ambil kredensial menggunakan System.getenv() atau hardcode untuk testing lokal
+        // - Buat instance DatabaseRepository dan MockEmailNotifier
+
+        // TODO: Setup Processor
+        // - Buat instance PaymentProcessor dan injeksi services tersebut
+
+        // TODO: Test Legacy Payment
+        // - Buat objek CreditCardPayment dan proses pembayarannya
+
+        // TODO: Test Fitur Baru
+        // - Buat objek GoPayPayment dan proses pembayarannya
+    }
+}
