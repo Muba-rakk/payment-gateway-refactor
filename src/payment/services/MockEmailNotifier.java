6de2a67d4f976a051cdc0769646c72f139c85d6b
Ariiq <riixxffzz@gmail.com>
2026-06-09 15:24:58 +0700

    Revert Main.java, MockEmailNotifier.java, and VS Code configs to match main branch

--- src/payment/services/MockEmailNotifier.java
+++ src/payment/services/MockEmailNotifier.java
@@ -3,15 +3,13 @@ package payment.services;
 import payment.interfaces.PaymentNotifier;
 
 public class MockEmailNotifier implements PaymentNotifier {
-    private String smtpServer;
+    // TODO: Deklarasikan private String smtpServer
 
-    public MockEmailNotifier(String smtpServer) {
-        this.smtpServer = smtpServer;
-    }
+    // TODO: Buat constructor untuk menginisialisasi smtpServer
 
     @Override
     public void sendNotification(double amount, String recipient) {
-        String message = String.format("Pembayaran sebesar Rp%.2f sukses dikirim ke %s melalui server %s.", amount, recipient, smtpServer);
+        // TODO: Print simulasi pesan pengiriman email
         System.out.println("[EMAIL NOTIFICATION] Mengirim email: " + message);
     }
 }
