46263b22a8e1dd938e8c7fff74df741f3b95b456
Winata Fadillah Mubarak <winatafadil1177@gmail.com>
2026-06-09 20:08:26 +0700

    fix(services): implementasi mockemailnotifier dan perbaikan error

--- src/payment/services/MockEmailNotifier.java
+++ src/payment/services/MockEmailNotifier.java
@@ -3,13 +3,15 @@ package payment.services;
 import payment.interfaces.PaymentNotifier;
 
 public class MockEmailNotifier implements PaymentNotifier {
-    // TODO: Deklarasikan private String smtpServer
+    private String smtpServer;
 
-    // TODO: Buat constructor untuk menginisialisasi smtpServer
+    public MockEmailNotifier(String smtpServer) {
+        this.smtpServer = smtpServer;
+    }
 
     @Override
     public void sendNotification(double amount, String recipient) {
-        // TODO: Print simulasi pesan pengiriman email
-        System.out.println("[EMAIL NOTIFICATION] Mengirim email: " + message);
+        String message = String.format("Pembayaran sebesar Rp%.2f berhasil.", amount);
+        System.out.println("[EMAIL NOTIFICATION] Mengirim email via " + smtpServer + " ke " + recipient + ": " + message);
     }
 }
